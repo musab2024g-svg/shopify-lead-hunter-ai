28af267a3c20513403bd346156316510fb1527da
musab2024g-svg <musab2024g@gmail.com>
2026-06-23 01:31:58 +0200

    Update gemini_test.py

--- gemini_test.py
+++ gemini_test.py
@@ -3,7 +3,8 @@ from settings import GEMINI_API_KEY
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 
 def ask_gemini(prompt):
     response = model.generate_content(prompt)
